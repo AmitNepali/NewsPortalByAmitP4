a34692dff9994d7e38479a63d7e8609ac2ccde96
AmitNepali <103407213+AmitNepali@users.noreply.github.com>
2022-04-30 12:24:48 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// live date part
+// let currentDate = new Date();
+// let month = currentDate.getMonth();
+// let day = currentDate.getDay();
+// let year = currentDate.getFullYear();
+// let fullDate = month + "/" + day + "/" + year ;
+// document.write("Today: ", fullDate);
